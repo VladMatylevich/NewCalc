4e890df1326f5fe690a844f735f8e598ba09af2f
Vlad Matylevich <125118544+VladMatylevich@users.noreply.github.com>
2023-06-10 12:48:40 +0300

    NewCalculator1.java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
-        String exp = scn.nextLine();
+        String exp = scn.nextLine(); // Считываем выражение с консоли
         char action;
         String[] data;
+
+// Определяем знак действия и разделяем операнды
         if (exp.contains(" + ")) {
             data = exp.split(" \\+ ");
             action = '+';
@@ -20,13 +22,32 @@ public class Main {
         } else {
             throw new Exception("Некорректный знак действия");
         }
+
+// Проверяем условия для операций умножения и деления
         if (action == '*' || action == '/') {
-            if (data[1].contains("\"")) throw new Exception("Строчку можно делить или умножать только на число");
+            if (!data[0].contains("\"")) {
+                throw new Exception("Первый операнд должен быть строкой, заключенной в кавычки");
+            }
+            if (!data[1].matches("\\d+")) {
+                throw new Exception("Второй операнд должен быть числом");
+            }
         }
+
+// Удаляем кавычки из операндов
         for (int i = 0; i < data.length; i++) {
             data[i] = data[i].replace("\"", "");
         }
 
+// Проверяем длину операндов
+        if (data[0].length() < 1 || data[0].length() > 10) {
+            throw new Exception("Первый операнд должен быть строкой длиной от 1 до 10 символов");
+        }
+
+        if (data[1].length() < 1 || data[1].length() > 10) {
+            throw new Exception("Второй операнд должен быть строкой длиной от 1 до 10 символов");
+        }
+
+// Выполняем действие в зависимости от знака
         if (action == '+') {
             printInQuotes(data[0] + data[1]);
         } else if (action == '*') {
@@ -50,14 +71,14 @@ public class Main {
             String result = data[0].substring(0, newLen);
             printInQuotes(result);
         }
-
-
     }
 
-    static void printInQuotes(String result) {
-        if (result.length() > 40) {
-            result = result.substring(0, 40) + "...";
-            System.out.println("\"" + result + "\"");
+    // Выводим результат в кавычках, с учетом ограничения на длину строки
+    static void printInQuotes(String text) {
+        if (text.length() > 40) {
+            System.out.println("\"" + text.substring(0, 40) + "..." + "\"");
+        } else {
+            System.out.println("\"" + text + "\"");
         }
     }
 }
